ccb384c7ddd28966d0733444e677983103596cfa
Om <115257951+G-Om@users.noreply.github.com>
2023-03-01 18:08:03 +0530

    commiting backend initial

---
+++ Backend/Ecom_Inventory/src/main/java/com/Fullstack/Ecom_Inventory/EcomInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.Fullstack.Ecom_Inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomInventoryApplication.class, args);
+	}
+
+}
---
+++ Backend/Ecom_Inventory/src/test/java/com/Fullstack/Ecom_Inventory/EcomInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Fullstack.Ecom_Inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
